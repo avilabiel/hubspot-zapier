e5fa873cbb16e5c40a2c1bb58fc150537553e969
Gabriel Ávila <avilabiel@gmail.com>
2021-04-30 14:59:06 -0300

    so now we have try catches

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -6,38 +6,55 @@ class ContactController {
   static async create(req, res) {
     const contact = new Contact(req.body);
 
-    const hubspotContact = await Hubspot.createContact(
-      Hubspot.buildFromContact(contact)
-    );
-    contact.hubspotId = hubspotContact.id;
-    const persistedContact = await ContactModel.create(contact);
+    try {
+      const hubspotContact = await Hubspot.createContact(
+        Hubspot.buildFromContact(contact)
+      );
+      contact.hubspotId = hubspotContact.id;
+      const persistedContact = await ContactModel.create(contact);
 
-    return res.send({
-      contact: persistedContact,
-    });
+      return res.send({
+        contact: persistedContact,
+      });
+    } catch (error) {
+      console.log(error);
+
+      return res
+        .status(500)
+        .send({ success: false, message: "Internal Server Error" });
+    }
   }
 
   static async update(req, res) {
     const { id: contactId } = req.params;
-    const persistedContact = await ContactModel.findOne({
-      where: { id: contactId },
-    });
-    const contact = new Contact(persistedContact);
 
-    await Hubspot.updateContact(Hubspot.buildFromContact(contact));
-    for (let key in req.body) {
-      if (!persistedContact[key]) {
-        continue;
+    try {
+      const persistedContact = await ContactModel.findOne({
+        where: { id: contactId },
+      });
+      const contact = new Contact(persistedContact);
+
+      await Hubspot.updateContact(Hubspot.buildFromContact(contact));
+      for (let key in req.body) {
+        if (!persistedContact[key]) {
+          continue;
+        }
+
+        persistedContact[key] = req.body[key];
       }
 
-      persistedContact[key] = req.body[key];
+      await persistedContact.save();
+
+      return res.send({
+        contact: persistedContact,
+      });
+    } catch (error) {
+      console.log(error);
+
+      return res
+        .status(500)
+        .send({ success: false, message: "Internal Server Error" });
     }
-
-    await persistedContact.save();
-
-    return res.send({
-      contact: persistedContact,
-    });
   }
 }
 
